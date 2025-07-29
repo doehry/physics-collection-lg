3937ea776ddd69cc0e078bc2e768c32da9d8795e
doehry <doehry@gmail.com>
2025-07-29 19:26:47 +0200

    cosmetic fixes

--- wp-content/themes/physics-collection-lg/archive.php
+++ wp-content/themes/physics-collection-lg/archive.php
@@ -2,11 +2,9 @@
 /**
  * The template for displaying archive pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
+ * @subpackage physics-collection-lg
+ * @since physics-collection-lg 0.1
  */
 
 get_header();
